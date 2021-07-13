727f4ecf4d8602fc0761f087d0f9b50f7e525420
13ruceYu <bruceyuhongbo@gmail.com>
2021-07-13 23:40:53 +0800

    study: java

--- GeekBangTuto/part_2/code029/src/com/geekbang/RunLittleSuperMarketApp.java
+++ GeekBangTuto/part_2/code029/src/com/geekbang/RunLittleSuperMarketApp.java
@@ -11,5 +11,28 @@ public class RunLittleSuperMarketApp {
         littleSuperMarket.parkingCount = 200;
         littleSuperMarket.merchandises = new Merchandise[10];
         littleSuperMarket.merchandiseSold = new int[littleSuperMarket.merchandises.length];
+
+        Merchandise[] all = littleSuperMarket.merchandises;
+
+        for (int i = 0; i < all.length; i++) {
+            Merchandise m = new Merchandise();
+            m.count = 200;
+            m.id = "ID" + i;
+            m.name = "商品" + i;
+            m.purchasePrice = Math.random() * 200;
+            m.soldPrice = (1 + Math.random()) * 200;
+            all[i] = m;
+        }
+
+        System.out.println("超市开门啦！");
+
+        boolean open = true;
+        while (open) {
+            System.out.println("本店叫做" + littleSuperMarket.superMarketName);
+            System.out.println("本店地址：" + littleSuperMarket.address);
+            System.out.println("共有停车位" + littleSuperMarket.parkingCount + "个");
+            System.out.println("今天营业额为" + littleSuperMarket.incomingSum);
+            System.out.println("共有商品" + littleSuperMarket.merchandises.length + "中");
+        }
     }
 }
