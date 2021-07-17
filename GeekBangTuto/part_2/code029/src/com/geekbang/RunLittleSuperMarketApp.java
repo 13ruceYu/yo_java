8711caaa7615f080e4efa7a307cd2b2ff1d75437
13ruceYu <bruceyuhongbo@gmail.com>
2021-07-17 23:56:28 +0800

    study: java

--- GeekBangTuto/part_2/code029/src/com/geekbang/RunLittleSuperMarketApp.java
+++ GeekBangTuto/part_2/code029/src/com/geekbang/RunLittleSuperMarketApp.java
@@ -1,7 +1,10 @@
 package com.geekbang;
 
+import com.geekbang.person.Customer;
 import com.geekbang.supermaket.LittleSuperMarket;
 import com.geekbang.supermaket.Merchandise;
+
+import java.util.Scanner;
 
 public class RunLittleSuperMarketApp {
     public static void main(String[] args) {
@@ -27,12 +30,75 @@ public class RunLittleSuperMarketApp {
         System.out.println("超市开门啦！");
 
         boolean open = true;
+        Scanner scanner = new Scanner(System.in);
         while (open) {
             System.out.println("本店叫做" + littleSuperMarket.superMarketName);
             System.out.println("本店地址：" + littleSuperMarket.address);
             System.out.println("共有停车位" + littleSuperMarket.parkingCount + "个");
             System.out.println("今天营业额为" + littleSuperMarket.incomingSum);
             System.out.println("共有商品" + littleSuperMarket.merchandises.length + "中");
+
+            Customer customer = new Customer();
+            customer.name = "顾客编号" + ((int)(Math.random() * 10000));
+            customer.money = (1 + Math.random()) * 1000;
+            customer.isDrivingCar = Math.random() > 0.5;
+
+            if (customer.isDrivingCar) {
+                if (littleSuperMarket.parkingCount > 0) {
+                    System.out.println("欢迎" + customer.name + "驾车而来，停车免费，停车位为：" + littleSuperMarket.parkingCount);
+                    littleSuperMarket.parkingCount--;
+                } else {
+                    System.out.println("停车位已满，欢迎下次光临。");
+                }
+            } else {
+                System.out.println("欢迎" + customer.name + "来店选购。");
+            }
+
+            double totalCost = 0;
+            while (true) {
+                System.out.println("本店提供" + all.length + "种商品，欢迎选购。请输入商品编号：");
+                int index = scanner.nextInt();
+
+                if (index < 0) {
+                    break;
+                }
+
+                Merchandise m = all[index];
+
+                System.out.println("您选购的商品名字" + m.name + "，单价是" + m.soldPrice + "，请输入购买数量：");
+                int numToBuy = scanner.nextInt();
+
+                totalCost += numToBuy * m.soldPrice;
+
+                m.count -= numToBuy;
+                littleSuperMarket.merchandiseSold[index] += numToBuy;
+            }
+
+            customer.money -= totalCost;
+
+            if (customer.isDrivingCar) {
+                littleSuperMarket.parkingCount++;
+            }
+
+            System.out.println("顾客" + customer.name + "共消费" + totalCost);
+
+            littleSuperMarket.incomingSum = totalCost;
+
+            System.out.println("是否继续营业？");
+            open = scanner.nextBoolean();
+        }
+
+        System.out.println("超市关门了！");
+        System.out.println("今日营业额为" + littleSuperMarket.incomingSum + "，营业情况如下：");
+
+        for (int i = 0; i < littleSuperMarket.merchandiseSold.length; i++) {
+            Merchandise m = all[i];
+            int numSold = littleSuperMarket.merchandiseSold[i];
+            if (numSold > 0) {
+                double incoming = m.soldPrice * numSold;
+                double netIncoming = (m.soldPrice - m.soldPrice) * numSold;
+                System.out.println(m.name + "售出了" + numSold + "个，销售额为" + incoming + "。净利润为" + netIncoming);
+            }
         }
     }
 }
